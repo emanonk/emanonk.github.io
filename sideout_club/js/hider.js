73adb94496e75fdb0da4fc9f9908daf4ad48b15d
mmkamm <manos.kammas@camelotglobal.com>
2019-03-03 23:54:32 +0000

    calendar for leyton is ready

--- sideout_club/js/hider.js
+++ sideout_club/js/hider.js
@@ -15,6 +15,8 @@ var shortContent1 = document.getElementById('short-content1')
 var shortContent2 = document.getElementById('short-content2')
 var shortContent3 = document.getElementById('short-content3')
 var beachTitle = document.getElementById('beachHeader')
+var locationHeader = document.getElementById('locationHeader')
+var map = document.getElementById('google-map')
 
 var part1 = '<div id="venueBooking" style="top:;bottom:;left:;right:;height:1200px;position:" class="htco1" data-state="hasContent" id="HtmlCmpnnt0-apz">\
   <div id="HtmlCmpnnt0-apziFrameHolder" class="htco1iFrameHolder">\
@@ -22,55 +24,50 @@ var part1 = '<div id="venueBooking" style="top:;bottom:;left:;right:;height:1200
 
 var part2 = '></iframe></div></div>';
 
+var mapLeyton = '<iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=51.5630325,-0.01934853&amp;q=E10%205HQ+(Sideout%20Beach)&amp;ie=UTF8&amp;t=&amp;z=17&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/map-my-route/">Create a route on google maps</a></iframe>';
+var mapWorthing = '<iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=BN11%202FG+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/map-my-route/">Draw map route</a></iframe>';
+
 var shortContentLeyton1 = '\
-<h3 class="entry-title">Opening Times & Bookings</h3>\
-\
-<p>From 1 April to 30 September, our courts are available to book between 10:00am and 10:00pm on weekdays. Weekend bookings are not available.\
+From 1 April to 30 September, our courts are available to book between 10:00am and 10:00pm on weekdays. Weekend bookings are not available.\
 <br /><br />\
 ​Courts can be booked up to 30 days in advance.\
 <br /><br />\
-For any block bookings beyond 30 days, corporate bookings or other private events, please feel free to contact us via the form at the bottom of this page!</p>'
+For any block bookings beyond 30 days, corporate bookings or other private events, please feel free to contact us via the form at the bottom of this page!'
 
 var shortContentLeyton2 = '\
-<h3 class="entry-title">Rates & Payment</h3>\
-<p>Standard rate:  £21 per hour (maximum of 8 people per court)\
+Standard rate:  £21 per hour (maximum of 8 people per court)\
 <br /><br />\
 Members\' rate: £16 per hour (maximum of 8 people per court). Available only to members of SideOut Beach Volleyball Club. \
 <br /><br />\
-Payment: We only accept payment through PayPal, which offers maximum flexibility and ensures that your payments are secure! No additional charges apply and you do not need a PayPal account to make payment. </p>'
+Payment: We only accept payment through PayPal, which offers maximum flexibility and ensures that your payments are secure! No additional charges apply and you do not need a PayPal account to make payment.'
 
 var shortContentLeyton3 = '\
-<h3 class="entry-title">Address & Directions</h3>\
-<p>Public Transport: Access is via Ive Farm Close, E10 5HQ. This is a 20-minute walk from Leyton underground station or a 15-minute walk from Leyton Midland Road overground station\
+Public Transport: Access is via Ive Farm Close, E10 5HQ. This is a 20-minute walk from Leyton underground station or a 15-minute walk from Leyton Midland Road overground station\
 <br /><br />\
-Driving: The venue car park can be accessed only from Orient Way. There is no postcode for this location yet, so please use the following on your SatNav: 51°33\'39.7"N 0°01\'18.0"W  or  51.561022, -0.021670. </p>'
+Driving: The venue car park can be accessed only from Orient Way. There is no postcode for this location yet, so please use the following on your SatNav: 51°33\'39.7"N 0°01\'18.0"W  or  51.561022, -0.021670.'
 
 var shortContentWorthing1 = '\
-<h3 class="entry-title">Opening Times & Bookings</h3>\
-\
-<p>From 1 April to 30 September, our courts are available to book between 10:00am and 10:00pm on weekdays. Weekend bookings are not available.\
+From 1 April to 30 September, our courts are available to book between 10:00am and 10:00pm on weekdays. Weekend bookings are not available.\
 <br /><br />\
 ​Courts can be booked up to 30 days in advance.\
 <br /><br />\
-For any block bookings beyond 30 days, corporate bookings or other private events, please feel free to contact us via the form at the bottom of this page!</p>'
+For any block bookings beyond 30 days, corporate bookings or other private events, please feel free to contact us via the form at the bottom of this page!'
 
 
 var shortContentWorthing2 = '\
-<h3 class="entry-title">Rates & Payment</h3>\
 <p>Standard rate:  £21 per hour (maximum of 8 people per court)\
 <br /><br />\
 Members\' rate: £16 per hour (maximum of 8 people per court). Available only to members of SideOut Beach Volleyball Club. \
 <br /><br />\
-Payment: We only accept payment through PayPal, which offers maximum flexibility and ensures that your payments are secure! No additional charges apply and you do not need a PayPal account to make payment. </p>'
+Payment: We only accept payment through PayPal, which offers maximum flexibility and ensures that your payments are secure! No additional charges apply and you do not need a PayPal account to make payment.'
 
 
 var shortContentWorthing3 = '\
-<h3 class="entry-title">Address & Directions</h3>\
 <p>Worthing Beach Volleyball Courts, BN11 2FG. This is a 20-minute walk from Leyton underground station or a 15-minute walk from Leyton Midland Road overground station\
 <br /><br />\
 Driving or public transport: Access is via Brighton Road (A259).\
 <br /><br />\
-Cycling or walking: You can also access us via Beach Parade, the Worthing seafront promenade.</p>'
+Cycling or walking: You can also access us via Beach Parade, the Worthing seafront promenade.'
 
 
 var beachTitleLeyton = '<header class="entry-header elements-heading"><h2 class="entry-title">Leyton Beach</h2></header>'
@@ -80,16 +77,20 @@ var beachTitleWorthing = '<header class="entry-header elements-heading"><h2 clas
 var link = '';
 if( venue == 'leytonBooking'){
   beachTitle.innerHTML = beachTitleLeyton
+  locationHeader.innerHTML = 'Leyton Beach Location'
   link= 'src="https://www-sideout-co-uk.filesusr.com/html/a1273a_8b9c89bc80e83f8ae0363b4d501a0396.html"';
   shortContent1.innerHTML = shortContentLeyton1;
   shortContent2.innerHTML = shortContentLeyton2;
   shortContent3.innerHTML = shortContentLeyton3;
+  map.innerHTML = mapLeyton;
 }else{
   beachTitle.innerHTML = beachTitleWorthing
+  locationHeader.innerHTML = 'Worthing Beach Location'
   link = 'src="https://www-sideout-co-uk.filesusr.com/html/a1273a_31379c57d27a21ec3f54f346939fc991.html"';
   shortContent1.innerHTML = shortContentWorthing1;
   shortContent2.innerHTML = shortContentWorthing2;
   shortContent3.innerHTML = shortContentWorthing3;
+  map.innerHTML = mapWorthing;
 }
 
 
@@ -103,12 +104,12 @@ d1.innerHTML = element;
 
 ////////////////////////////////////////////
 
-function firstRender() {
-  var link = 'src="https://www-sideout-co-uk.filesusr.com/html/a1273a_8b9c89bc80e83f8ae0363b4d501a0396.html"';
-  var element = part1 + link + part2;
-  document.getElementById("venue1").innerHTML = element;
+// function firstRender() {
+//   var link = 'src="https://www-sideout-co-uk.filesusr.com/html/a1273a_8b9c89bc80e83f8ae0363b4d501a0396.html"';
+//   var element = part1 + link + part2;
+//   document.getElementById("venue1").innerHTML = element;
 
-}
+// }
 
 
 
